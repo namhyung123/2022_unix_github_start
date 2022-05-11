59c12f8e83a0d11d4727a862b9f068f654e21e2c
masinda <developjaeyong@gmail.com>
2022-05-11 10:14:20 +0900

    new/mul

--- calc.c
+++ calc.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
-int add(int a, int b);
+long long add(int a, int b);
+long long mul(int a, int b);
+long long sub(int a, int b);
 
 int main() {
+	char c;
 	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d + %d = %d\n", a, b, add(a, b));
+	scanf("%c %d %d", &c, &a, &b);
+	long long result = 0;
+	switch(c){
+		case '+':
+			result = add(a,b);
+			printf("%d + %d = %lld\n", a, b, add(a, b));
+			break;
+		case '-':
+			result = sub(a,b);
+			printf("%d - %d = %lld\n", a, b, sub(a, b));
+			break;
+		case '*':
+			result = mul(a,b);
+			printf("%d * %d = %lld\n", a, b, mul(a, b));
+			break;
+	}
 }
 
---
+++ mul.c
@@ -0,0 +1,4 @@
+
+long long mul(int a, int b){
+	return (long long) a*b;
+}
